c5a800d5098cc5ba56c28a148f882af2ef7690ae
김지헌 <youwa65@gmail.com>
2023-01-25 21:48:05 +0900

    몽구스

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,9 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { CatsModule } from './cats/cats.module';
 import { UsersModule } from './users/users.module';
-
+import { MongooseModule } from '@nestjs/mongoose'
 @Module({
-  imports: [CatsModule, UsersModule],
+  imports: [CatsModule, UsersModule, MongooseModule.forRoot('mongodb://localhost/nest')],
   controllers: [AppController],
   providers: [AppService],
 })
