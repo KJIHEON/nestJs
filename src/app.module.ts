5e0fc4a1d816c100f289dd0a86c9db37594caa7e
김지헌 <youwa65@gmail.com>
2023-01-30 17:14:27 +0900

    몽고

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,18 @@
-import { Module } from '@nestjs/common';
+import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { CatsModule } from './cats/cats.module';
 import { UsersModule } from './users/users.module';
+import {LoggerMiddleware} from 'comm'
 import { MongooseModule } from '@nestjs/mongoose'
+
 @Module({
   imports: [CatsModule, UsersModule, MongooseModule.forRoot('mongodb://localhost/nest')],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule implements NestModule{
+  configure(consumer: MiddlewareConsumer) {
+     consumer.apply(LoggerMiddleware).forRoutes('*')
+  }
+}
